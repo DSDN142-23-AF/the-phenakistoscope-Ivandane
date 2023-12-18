077e72e64c29a7ec86f4e34d2c509af9ad827669
Ivandane Mangubat <mangubivan@gmail.com>
2023-12-19 10:48:40 +1300

    Added bubble tea and layout
    Added bubble tea and pearl images, edited layout of the pScope.

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,49 +1,71 @@
-const SLICE_COUNT = 10;
+// NUMBER OF SLICES
+const SLICE_COUNT = 12;
 
+// SETUP PSCOPE FUNCTION
 function setup_pScope(pScope){
-  pScope.output_mode(ANIMATED_DISK);
+  //pScope.output_mode(ANIMATED_FRAME);
+  pScope.output_mode(OUTPUT_GIF(1000));
   pScope.scale_for_screen(true);
-  pScope.draw_layer_boundaries(false);
+  pScope.draw_layer_boundaries(true); // Set to false before hand-in
   pScope.set_direction(CCW);
   pScope.set_slice_count(SLICE_COUNT);
+
+  // Load Image
+  pScope.load_image('bubble-tea', 'png');
+  pScope.load_image('pearl', 'png');
 }
 
 function setup_layers(pScope){
+  // Draw Circle Background
+  new PLayer(null, 170);
 
-  new PLayer(null, 220);  //lets us draw the whole circle background, ignoring the boundaries
+  // Inner Circle Layer
+  let innerCircleLayer = new PLayer(innerCircle);
+  innerCircleLayer.mode(RING);
+  innerCircleLayer.set_boundary(0, 400);
 
-  var layer1 = new PLayer(faces);
-  layer1.mode( SWIRL(5) );
-  layer1.set_boundary( 200, 1000 );
+  // Pearl Layer
+  let pearlLayer = new PLayer(pearl);
+  pearlLayer.mode(SWIRL(1));
+  pearlLayer.set_boundary(0, 400);
 
-  var layer2 = new PLayer(squares);
-  layer2.mode( RING );
-  layer2.set_boundary( 0, 400 );
+  // Bubble Tea Layer
+  let bubbleTeaLayer = new PLayer(bubbleTea);
+  bubbleTeaLayer.mode(RING);
+  bubbleTeaLayer.set_boundary(0, 400);
+
+  // Outer Circle Layer
+  let outerCircleLayer = new PLayer(outerCircle);
+  outerCircleLayer.mode(RING);
+  outerCircleLayer.set_boundary(800, 1000);
+
+  // Ice Cube Layer
 }
 
-function faces(x, y, animation, pScope){
-  
-  scale(animation.frame*2);
-
-  ellipse(0,0,50,50); // draw head
-  fill(30);
-  ellipse(-10,-10,10,10); //draw eye
-  ellipse(10,-10,10,10); // draw eye
-  arc(0,10,20,10,0,180); // draw mouth
-
+// INNER CIRCLE FUNCTION
+function innerCircle(x, y, animation, pScope) {
+  pScope.fill_background(3, 186, 252, 255);
 }
 
-function squares(x, y, animation, pScope){
+// BUBBLE TEA FUNCTION
+function bubbleTea(x, y, animation, pScope) {
+  push();
+    translate(0, -400);
+    scale(1.25);
+    pScope.draw_image('bubble-tea', x, y - (animation.wave(0.5) * 150));
+  pop();
+}
 
-  // this is how you set up a background for a specific layer
-  let angleOffset = (360 / SLICE_COUNT) / 2
-  let backgroundArcStart = 270 - angleOffset;
-  let backgroundArcEnd = 270 + angleOffset;
+// PEARL FUNCTION
+function pearl(x, y, animation, pScope) {
+  push();
+    scale(map(animation.frame, 0, 1, 0.1, 0.75));
+    rotate(animation.frame * 360);
+    pScope.draw_image('pearl', x, y);
+  pop();
+}
 
-  fill(66, 135, 245)
-  arc(x,y,800,800,backgroundArcStart,backgroundArcEnd); // draws "pizza slice" in the background
-
-  fill(255)
-  rect(-10,-300-animation.wave()*50,20,20) // .wave is a cosine wave btw
-
+// OUTER CIRCLE FUNCTION
+function outerCircle(x, y, animation, pScope) {
+  pScope.fill_background(3, 186, 252, 255);
 }
